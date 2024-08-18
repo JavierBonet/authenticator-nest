5cb1f0082378f31e634ede8ecf86a6005bec590d
Javier Bonet <javbonet@gmail.com>
2024-08-18 20:59:10 -0300

    Removes old routers and controllers

--- backend/src/routes/moviesRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import MoviesController from "../controllers/moviesController";
-
-const router = express.Router();
-const moviesController = new MoviesController();
-
-router.get("/", (req, res) => moviesController.getMovies(req, res));
-
-export { router };
