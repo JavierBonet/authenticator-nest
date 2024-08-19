ecdb60bb144c1517eb3c66020ecc2fa034ec77e0
Javier Bonet <javbonet@gmail.com>
2024-08-18 21:04:22 -0300

    Update start file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,4 @@
 import "dotenv/config";
-import DB from "./db";
 import { startServer } from "./server";
 
-(async () => {
-  const db = DB.getInstance();
-  await db.connect();
-
-  startServer();
-})();
+(async () => await startServer())();
