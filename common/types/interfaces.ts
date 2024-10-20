0cb86c04a25fe48b291e6bff92126b9140b3953b
Javier Bonet <javbonet@gmail.com>
2024-10-20 20:06:46 -0300

    Adds back needed type entities

--- common/types/interfaces.ts
+++ common/types/interfaces.ts
@@ -1,6 +1,18 @@
 import type { ObjectId } from "../../backend/node_modules/mongodb";
 import { JwtPayload } from "jsonwebtoken";
 
+export interface Movie {
+  _id: ObjectId;
+  title: string;
+  director: Director;
+  year: number;
+  genre: string[];
+  rating: number;
+  cast: CastEntity[];
+  plot_summary: string;
+  locations: LocationsEntity[];
+}
+
 export interface Director {
   name: string;
   nationality: string;
@@ -23,6 +35,16 @@ export interface LocationsEntity {
   description: string;
 }
 
+export interface Product {
+  _id: ObjectId;
+  name: string;
+  manufacturer: Manufacturer;
+  category: string;
+  description: string;
+  specs: Specs;
+  usage_tips: string[];
+}
+
 export interface Manufacturer {
   name: string;
   country: string;
@@ -31,6 +53,15 @@ export interface Manufacturer {
 }
 
 type ProductsEntity = Record<string, string | undefined>;
+
+export interface ProgrammingLanguage {
+  name: string;
+  developer: string;
+  first_appeared: number;
+  paradigm: string[];
+  typing_discipline: string;
+  usage: string[];
+}
 
 export type Specs = Record<string, string | string[] | undefined>;
 
